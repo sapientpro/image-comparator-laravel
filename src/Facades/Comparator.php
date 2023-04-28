866810d8d123f9de3f2116211d99745f986cad1a
Vadim Matsotskyi <vadim.matsotskyi@sapient.pro>
2023-04-28 13:41:29 +0300

    - changed underlying facade service

--- src/Facades/Comparator.php
+++ src/Facades/Comparator.php
@@ -5,25 +5,12 @@ namespace SapientPro\ImageComparatorLaravel\Facades;
 use GdImage;
 use Illuminate\Support\Facades\Facade;
 use SapientPro\ImageComparator\Enum\ImageRotationAngle;
-use SapientPro\ImageComparatorLaravel\ComparatorService;
+use SapientPro\ImageComparator\ImageComparator;
 
-/**
- * @method static float|array detectImageSimilarity(string|GdImage $sourceImage, string|array $comparedImage, int $precision = 1)
- * @method static float|array compareImages(string|GdImage $sourceImage, string|array $comparedImage, ImageRotationAngle $rotationAngle = ImageRotationAngle::D0, int $precision = 1)
- * @method static void setAverageHashStrategy()
- * @method static void setDifferenceHashStrategy()
- * @method static GdImage|false squareImage(string $image)
- * @method static float|array compareSquareImages(string $sourceImage, string|array $images, ImageRotationAngle $rotationAngle = ImageRotationAngle::D0, int $precision = 1)
- * @method static array hashImage(string|GdImage $image, ImageRotationAngle $rotationAngle = ImageRotationAngle::D0, int $size = 8)
- * @method static string convertHashToBinaryString(array $hash)
- * @method static float compareHashStrings(string $hash1, string $hash2)
- *
- * @see ComparatorService
- */
 class Comparator extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return ComparatorService::class;
+        return ImageComparator::class;
     }
 }
--- src/Providers/ComparatorServiceProvider.php
+++ src/Providers/ComparatorServiceProvider.php
@@ -4,14 +4,11 @@ namespace SapientPro\ImageComparatorLaravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use SapientPro\ImageComparator\ImageComparator;
-use SapientPro\ImageComparatorLaravel\ComparatorService;
 
 class ComparatorServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(ComparatorService::class, function ($app) {
-            return new ComparatorService($app->make(ImageComparator::class));
-        });
+        //
     }
 }
