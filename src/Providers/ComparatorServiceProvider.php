f5a9fd3b74bc2c7b48cf04b15f807d89cb359c33
Vadim Matsotskyi <vadim.matsotskyi@sapient.pro>
2023-04-28 13:03:17 +0300

    - updated provider

--- src/Providers/ComparatorServiceProvider.php
+++ src/Providers/ComparatorServiceProvider.php
@@ -3,11 +3,15 @@
 namespace SapientPro\ImageComparatorLaravel\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use SapientPro\ImageComparator\ImageComparator;
+use SapientPro\ImageComparatorLaravel\ComparatorService;
 
 class ComparatorServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
-        //
+        $this->app->singleton(ComparatorService::class, function ($app) {
+            return new ComparatorService($app->make(ImageComparator::class));
+        });
     }
 }
